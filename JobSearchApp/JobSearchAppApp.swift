c8f45fd8634152b4ff6df5ea2b4e6f9d7f9bd9e8
Irek <yairek@gmail.com>
2024-03-15 09:46:03 +0100

    Initial Commit

---
+++ JobSearchApp/JobSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchAppApp.swift
+//  JobSearchApp
+//
+//  Created by Reek i on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
